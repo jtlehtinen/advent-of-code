2be666bc4109086394726dc588c09f7a807e0256
Juha Lehtinen <jtlehtinen@fastmail.com>
2021-12-14 13:06:45 +0200

    java: solve 2015/1

---
+++ 2015/java/1/Solution.java
@@ -0,0 +1,38 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+/**
+ * Problem statement: https://adventofcode.com/2015/day/1
+ */
+public class Solution {
+  private static int solvePart1(String in) {
+    int floor = 0;
+    for (char c : in.toCharArray()) {
+      if (c == '(') floor++;
+      else if (c == ')') floor--;
+    }
+    return floor;
+  }
+
+  private static int solvePart2(String in) {
+    int floor = 0;
+    for (int i = 0; i < in.length(); ++i) {
+      switch (in.charAt(i)) {
+        case '(': floor++; break;
+        case ')': floor--; break;
+      }
+      if (floor == -1) return i + 1;
+    }
+    throw new IllegalArgumentException("invalid input");
+  }
+
+  public static void main(String[] args) throws Exception {
+    var in = Files.readString(Path.of("./../../input/input1.txt")).trim();
+
+    var solution1 = solvePart1(in);
+    var solution2 = solvePart2(in);
+
+    System.out.println("solution part 1: " + solution1);
+    System.out.println("solution part 2: " + solution2);
+  }
+}
